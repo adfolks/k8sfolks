0701eaad25a0dc40eea7a305a3c65370b5cf0300
Anoop Lekshmanan <anpl1980@gmail.com>
2018-10-28 19:27:15 +0530

    Add functions for each task in the cluster

--- k8sfolks.go
+++ k8sfolks.go
@@ -1,7 +1,7 @@
-// An Interactive cli to start, stop and delete AKS Cluster
-// At ADfolks we required this to reduce the AKS billing costs
-// We use this to stop the Dev clusters when not in use
-// Or delete and start again with from scratch in a single command
+/* An Interactive cli to start, stop and delete AKS Cluster
+   At ADfolks we required this to reduce the AKS billing costs
+   We use this to stop the Dev clusters when not in use
+*/ Or delete and start again with from scratch in a single command
 
 package main
 
@@ -14,62 +14,115 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Start setiing up your k8s Cluster")
-	fmt.Println("---------------------------------")
-	readerName := bufio.NewReader(os.Stdin)
+func getClusterName() string {
+	readerClusterName := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter k8s cluster Name:")
-	clusterName, _ := readerName.ReadString('\n')
+	clusterName, _ := readerClusterName.ReadString('\n')
 	clusterName = strings.TrimSuffix(clusterName,"\n")
-	readerRegion := bufio.NewReader(os.Stdin)
+	return clusterName
+}
+
+func getClusterRegion() string {
+	readerClusterRegion := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter k8s cloud region:")
-	clusterRegion, _ := readerRegion.ReadString('\n')
+	clusterRegion, _ := readerClusterRegion.ReadString('\n')
 	clusterRegion = strings.TrimSuffix(clusterRegion, "\n")
-	//Create Azure Resource Group
-	resourceGroupName := clusterName
-	cmd := exec.Command("az", "group", "create", "-l", clusterRegion, "-n", resourceGroupName)
+	retun clusterRegion
+}
+func createResourceGroup(resourceGroupName string, clusterRegion string) {
+	cmd := exec.Command("az", "group", "create", "-l", clusterRegion, "-n",
+		 resourceGroupName)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-	log.Fatalf("cmd.Run() failed with %s\n", err)
-    }
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
 	fmt.Printf("Output:\n%s\n", string(out))
+}
+
+func createCluster( clusterName string) sting, string {
+	fmt.Println("Start setiing up your k8s Cluster")
+	fmt.Println("---------------------------------")
 	//Create AKS Cluster
-	cmd = exec.Command("az", "aks", "create", "--name", clusterName, "--resource-group", resourceGroupName, "--node-count", "6", "--kubernetes-version", "1.11.3")
+	cmd = exec.Command("az", "aks", "create", "--name", clusterName,
+		"--resource-group", resourceGroupName, "--node-count",
+		 "6", "--kubernetes-version", "1.11.3")
 	out, err = cmd.CombinedOutput()
 	if err != nil {
 	log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
     fmt.Printf("Output:\n%s\n", string(out))
+}
+
+func getKubectlConfig(clusterName string) {
 	//Get Kubectl credentials copied to ~/.kube/config
-	getKubectlConfig := exec.Command("az", "aks", "get-credentials", ""--resource-group", resourceGroupName, "--name",clusterName)
-	out, err = getKubectlConfig.CombinedOutput()
+	cmd := exec.Command("az", "aks", "get-credentials",
+		 "--resource-group", resourceGroupName, "--name",clusterName)
+	out, err = cmd.CombinedOutput()
 	if err != nil {
-	log.Fatalf("getKubectlConfig.Run() Failed with %s\n", err)
+	log.Fatalf("cmd.Run() Failed with %s\n", err)
 	}
 	fmt.Printf("Get kubectl config Output: \n%s\n", string(out))
-    //Initialize Helm
+}
+
+func intializeHelm() string {
+	//Initialize Helm
 	fmt.Println("Initialising Package manager Helm")
 	cmd = exec.Command("helm", "init")
-    out, err = cmd.CombinedOutput()
-    if err != nil {
-    log.Fatalf("cmd.Run() Failed with:\n%s\n", err)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() Failed with:\n%s\n", err)
 	}
 	fmt.Printf("Helm Init Output:\n%s\n", string(out))
-	//Install ElasticSearch
-	fmt.Println("Installing ElasticSearch with Helm")
+}
+
+func addHelmRepo(repoName string, repoURL string) string {
+	fmt.Println("Adding Helm repo: %s", repoName)
 	helmIncubatorRepo := "http://storage.googleapis.com/kubernetes-charts-incubator"
-	addRepo := exec.Command("helm", "repo", "add", "incubator", helmIncubatorRepo)
-    out, err = addRepo.CombineOutput()
-    if err != nil {
-        log.Fatalf("Add Helm Repo failed with: \n%s\n" err)
+	cmd := exec.Command("helm", "repo", "add", "incubator", helmIncubatorRepo)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+				log.Fatalf("Add Helm Repo failed with: \n%s\n", err)
+		}
+	fmt.Println("Helm Add repo output:\n%s\n", string(out))
+
+}
+func addPackage(packageName) string {
+	//Get Kubectl credentials copied to ~/.kube/config
+	cmd := exec.Command("az", "aks", "get-credentials",
+		 "--resource-group", resourceGroupName, "--name",clusterName)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+	log.Fatalf("cmd.Run() Failed with %s\n", err)
+	}
+	fmt.Printf("Get kubectl config Output: \n%s\n", string(out))
+}
+func createNamespace(nameSpace string) {
+	cmd := exec.Command("kubectl", "create", "namespace", "elasticsearch")
+	out, err = cmd.CombinedOutput()
+  if err != nil {
+        log.Fatalf("Kubectl create namespace failed with: \n%s\n", err)
     }
-    fmt.Println("Helm Add repo output:\n%s\n", string(out))
-	installPackage := exec.Command("helm", "install", "--name", "elasticsearch", "incubator/elasticsearch")
-    //Install Kafka
-    installPackage = exec.Command("helm", "install", "--name", "kafka", "incubator/kafka")
-    out, err = installPackage.CombinedOutput()
-    if err != nil {
-    log.Fatalf("cmd.run() Failed with:\n%s\n", err)
-    }
-    fmt.Println("Package install Output:\n%s\n", string(out))
+  fmt.Println("kubectl create namespace output:\n%s\n", string(out))
+
+}
+func main() {	 clusterRegion := getClusterRegion()
+   clusterName := getClusterName()
+	 //assign Azure Resource Group same name as cluster name
+	 resourceGroupName := clusterName
+
+
+	//Install ElasticSearch
+  //Install Kafka
+	createNamespace = exec.Command("kubectl", "create", "namespace", "kafka")
+	out, err = createNamespace.CombinedOutput()
+  if err != nil {
+        log.Fatalf("Kubectl create namespace Kafka failed with: \n%s\n", err)
+			}
+  fmt.Println("kubectl create namespace output:\n%s\n", string(out))
+  installPackage = exec.Command("helm", "install", "--name", "kafka", "--namespace", "kafka", "incubator/kafka")
+  out, err = installPackage.CombinedOutput()
+  if err != nil {
+		log.Fatalf("cmd.run() Failed with:\n%s\n", err)
+	}
+  fmt.Println("Package install Output:\n%s\n", string(out))
 }
